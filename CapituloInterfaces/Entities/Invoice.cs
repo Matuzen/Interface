c6c84317b03f1abce44151d743500e9b61b165ee
Matheus Alexandre <matheuscurses@gmail.com>
2022-11-21 18:02:43 -0300

    Projeto sem Interface

---
+++ CapituloInterfaces/Entities/Invoice.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace CapituloInterfaces.Entities
+{
+    class Invoice
+    {
+        public double BasicPayment { get; set; }
+        public double Tax { get; set; }
+        public Invoice(double basicPayment, double tax)
+        {
+            BasicPayment = basicPayment;
+            Tax = tax;
+        }
+
+        public double TotalPayment
+        {
+            get { return BasicPayment + Tax; }
+        }
+
+        public override string ToString()
+        {
+            return "Basice payment: "
+                + BasicPayment.ToString("F2",  CultureInfo.InvariantCulture)
+            +"Tax: "
+            + Tax.ToString("F2", CultureInfo.InvariantCulture)
+            +"Total payment: "
+            + TotalPayment.ToString("F2", CultureInfo.InvariantCulture);
+        }
+
+    }
+}
---
+++ CapituloInterfaces/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CapituloInterfaces
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           // Sem interface
+        }
+    }
+}
